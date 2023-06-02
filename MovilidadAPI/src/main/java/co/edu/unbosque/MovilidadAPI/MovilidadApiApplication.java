07b4b0795d8e4d1bb00dc4e7e4ef122b4c8f5454
Johan Silva <johansilva09@gmail.com>
2023-06-02 16:40:24 -0500

    Project MovilidadApi created

---
+++ MovilidadAPI/src/main/java/co/edu/unbosque/MovilidadAPI/MovilidadApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.MovilidadAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovilidadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovilidadApiApplication.class, args);
+	}
+
+}
